c03f4630a2222584c6d03aec0ec96518a9fee7e1
kaidornel <kai_dornel@hotmail.com>
2020-03-31 15:54:27 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,56 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
   end
+
+  get '/articles/new' do
+    #renders the new.erb view
+    erb :new
+  end
+
+  get '/articles' do
+    #grab all articles and store them in instance variables
+    @articles = Article.all
+    # binding.pry
+    erb :index
+  end
+
+  post '/articles' do
+    title = params[:title]
+    content = params[:content]
+    article = Article.create(title: title, content: content)
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    id = params[:id]
+    @article = Article.find(id)
+    erb :edit
+  end
+  
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find(id)
+    
+    erb :show
+  end
+
+  patch '/articles/:id' do
+    article = Article.find(params[:id])
+    content = params[:content]
+    title = params[:title]
+    article.update(title: title, content: content)
+    redirect "/articles/#{article.id}"
+  end
+
+  delete "/articles/:id" do 
+    @article = Article.find(params[:id])
+    @article.destroy 
+    redirect "/articles"
 end
+
+end
+# Create the get '/articles/:id' controller action. This action should use Active Record to grab the 
+# article with the id that is in the params and set it equal to @article. Then, it should render the 
+# show.erb view page. That view should use ERB to render the @article's title and content.
