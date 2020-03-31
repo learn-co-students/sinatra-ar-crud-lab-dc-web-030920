3e9c99e53144188427e6e7810c768d086ac9c7e0
github username <github email address>
2020-03-31 17:28:26 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 
 require_relative '../../config/environment'
 
+
 class ApplicationController < Sinatra::Base
+  
+  set :method_override, true
 
   configure do
     set :public_folder, 'public'
@@ -9,5 +12,57 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect "/articles"
   end
+
+  get '/articles' do
+    @articles = Article.all
+    
+    erb :index
+    # binding.pry
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+  
+  get '/articles/:id' do
+    @article_id = Article.find(params[:id])
+    erb :show
+  end
+
+
+  post '/articles' do 
+    @article = Article.create(title: params[:title], content: params[:content])
+    
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id/edit'do
+    @article_edit = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article_id = Article.find(params[:id])
+    @article_id.update(title: params[:title], content: params[:content])  
+    @article_id.save
+    redirect "/articles/#{@article_id.id}"
+  end
+
+  delete '/articles/:id' do
+
+    @article_id = Article.find(params[:id])
+    @article_id.destroy
+    redirect '/articles'
+
+  end
+
+
+
+
+
+
+  
+  
 end
